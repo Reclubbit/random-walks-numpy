ac23780cad0377ee547f169720b02599ab192260
Reclubbit <reclubbit@163.com>
2025-04-30 20:56:57 +0800

    将生成位移向量作为方法提取出来

--- random_walks_class.py
+++ random_walks_class.py
@@ -22,15 +22,13 @@ def validate_int(x: int | np.integer) -> None:
 class RandomWalk:
     """
     一个 N 维随机漫步模拟器。
-    
-    属性：
+    属性:
         dim (int): 随机漫步的维度，必须为正整数。
         seed (int, optional): 随机数种子（默认None，若设置，可保证随机过程的可复现性）。
-        
-    方法：
+    方法:
+        _generate_displacement_vectors: 生成位移向量，用于append_steps和 append_runs方法
         append_steps: 给当前的所有随机漫步全部增加 n 步。
         append_runs: 增加 n 个与当前漫步相同步数的随机漫步。
-    
     """
     def __init__(self, dim: int, seed: int = None):
         self.dim = dim   # 维度
@@ -44,45 +42,70 @@ class RandomWalk:
         self.rng = np.random.default_rng(seed=self.seed)  # numpy随机数生成器
         self.pos = np.zeros((1, 1, self.dim))  # 漫步初始位置
     
+    def _generate_displacement_vectors(self,
+                                       runs : int | np.integer,
+                                       nsteps : int | np.integer) -> np.ndarray:
+        """
+        生成每次每步的位移向量。
+        参数:
+            runs(int): 新增的漫步数量，必须为正整数，默认为 1
+            nsteps(int): 当前每次漫步的新增步数，必须为正整数，默认为 1
+        返回:
+            numpy数组: 形状(runs, nsteps, self.dim)
+        """
+        # 随机每次每步的移动维度和距离
+        dim_indices = self.rng.integers(0, self.dim, size=(runs, nsteps))
+        move_distances = np.random.choice((1, -1), size=(runs, nsteps))
+        
+        # 生成次数轴和步数轴的索引，
+        walks = np.arange(runs)
+        steps = np.arange(nsteps)
+        walk_indices = np.repeat(walks, nsteps).reshape(runs, nsteps)
+        step_indices = np.tile(steps, (runs, 1))
+        
+        # 使用数组索引对位移向量数组的两个轴进行全遍历，并对维度轴索引，在指定位置替换为移动距离
+        displacement_vectors = np.zeros((runs, nsteps, self.dim))  # 初始化位移向量
+        displacement_vectors[walk_indices, step_indices,
+                                dim_indices] = move_distances  # 位移向量
+        
+        return displacement_vectors
+    
     def append_steps(self,
                      nsteps : int | np.integer = 1) -> np.ndarray:
         """
         给当前的所有随机漫步全部增加 n 步。
-        
-        参数：
+        参数:
             nsteps(int): 当前每次漫步的新增步数，必须为正整数，默认为 1
+        返回:
+            numpy数组: 形状为(self.pos.shape[0],
+                            self.pos.shape[1] + nsteps,
+                            self.dim)
         """
-        
         # 参数检测
         validate_int(nsteps)
         
         RUNS = self.pos.shape[0]  # 当前漫步次数，在该方法内保持不变
         
-        # 随机每次每步的移动维度和距离
-        dim_indices = self.rng.integers(0, self.dim, size=(RUNS, nsteps))
-        move_distances = np.random.choice((1, -1), size=(RUNS, nsteps))
+        # 生成位移向量，并根据当前位置对每次漫步的第一步位移向量进行修正
+        displacement_vectors = self._generate_displacement_vectors(RUNS, nsteps)
+        displacement_vectors[:, 0:1] =  (self.pos[:, -1:, :]
+                                         + displacement_vectors[:, 0:1, :])
         
-        # 生成次数轴和步数轴的索引，使用数组索引对位移向量数组的两个轴进行全遍历，并对维度轴索引，在指定位置替换为移动距离
-        walks = np.arange(RUNS)
-        steps = np.arange(nsteps)
-        walk_indices = np.repeat(walks, nsteps).reshape(RUNS, nsteps)
-        step_indices = np.tile(steps, (RUNS, 1))
-        
-        displacement_vectors = np.zeros((RUNS, nsteps, self.dim))  # 初始化位移向量
-        displacement_vectors[walk_indices, step_indices, dim_indices] = move_distances  # 位移向量
-        displacement_vectors[:, 0:1] =  self.pos[:, -1:, :] + displacement_vectors[:, 0:1, :]
-        # 累加位移向量，获得每次每步的位置
+        # 累加位移向量，获得新增的每次每步的位置
         pos_without_start = displacement_vectors.cumsum(axis=1)
-        # 将每步位置插入在当前已有漫步之后
+        # 将新增的每次每步位置插入在当前漫步之后
         self.pos = np.concatenate((self.pos, pos_without_start), axis=1)
     
     def append_runs(self,
                     runs : int | np.integer = 1) -> np.ndarray:
         """
         增加 n 个与当前漫步相同步数的随机漫步。
-
-        参数：
+        参数:
             runs(int): 新增的漫步数量，必须为正整数，默认为 1
+        返回:
+            numpy数组: 形状为(self.pos.shape[0] + runs,
+                            self.pos.shape[1],
+                            self.dim)
         """
         # 参数检测
         validate_int(runs)
@@ -93,21 +116,12 @@ class RandomWalk:
             self.pos = np.concatenate((self.pos, np.zeros((runs, NSTEPS, self.dim))), axis=0)
             return
         
-        # 随机每次每步的移动维度和距离
-        dim_indices = self.rng.integers(0, self.dim, size=(runs, NSTEPS))
-        move_distances = np.random.choice((1, -1), size=(runs, NSTEPS))
+        displacement_vectors = self._generate_displacement_vectors(runs, NSTEPS)
         
-        # 生成次数轴和步数轴的索引，使用数组索引对位移向量数组的两个轴进行全遍历，并对维度轴索引，在指定位置替换为移动距离
-        walks = np.arange(runs)
-        steps = np.arange(NSTEPS)
-        walk_indices = np.repeat(walks, NSTEPS).reshape(runs, NSTEPS)
-        step_indices = np.tile(steps, (runs, 1))
-        
-        displacement_vectors = np.zeros((runs, NSTEPS, self.dim))  # 初始化位移向量
-        displacement_vectors[walk_indices, step_indices, dim_indices] = move_distances  # 位移向量
         # 累加位移向量，获得每次每步的位置
         pos_without_start = displacement_vectors.cumsum(axis=1)
-        # 将每步位置插入在当前已有漫步之后
+        
+        # 在每次每步位置前插入初始位置后，再添加到已有漫步中
         start_pos = np.zeros((runs, 1, self.dim))
         pos_with_start = np.concatenate([start_pos, pos_without_start], axis=1)
         self.pos = np.concatenate((self.pos, pos_with_start), axis=0)
